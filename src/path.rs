97864baa593846f68009881069e82bfa33d42f19
Yukari Chiba <i@0x7f.cc>
2026-09-18 17:29:10 +0800

    save album and song meta

--- src/path.rs
+++ src/path.rs
@@ -51,6 +51,14 @@ pub fn init() -> std::io::Result<()> {
     Ok(())
 }
 
+pub fn get_music_meta_path(id: u64) -> PathBuf {
+    CACHE.join(format!("music_{}.meta.json", id))
+}
+
+pub fn get_album_meta_path(id: u64) -> PathBuf {
+    CACHE.join(format!("album_{}.meta.json", id))
+}
+
 pub fn get_music_cache_path(id: u64, rate: u32) -> PathBuf {
     let rate = match rate {
         0 => 128000,
